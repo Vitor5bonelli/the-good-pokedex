041235e3a9651a78e61ef967f6bf2b20dba90572
Vitor Bonelli <bonelli5.vitor@gmail.com>
2024-10-28 16:48:15 -0300

    build: add tailwind to project

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Hello from './pages/helloWorld.tsx'
-import "./main.css";
+import "./index.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/pages/helloWorld.tsx
+++ src/pages/helloWorld.tsx
@@ -1,7 +1,11 @@
-export default function Hello(){
-    return(
-        <>
-            <div>Hello World</div>
-        </>
-    )
+
+export default function Hello() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );  
 }
+
+
+
